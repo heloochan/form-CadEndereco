4ef017f9fc9cf1d6a9261c754a791f48c1312a4f
Heloise <heloseokjin@gmail.com>
2024-09-20 07:35:02 -0300

    Modificação no campo de endereço.

--- main.js
+++ main.js
@@ -16,3 +16,12 @@ const limparFormulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('logradouro').value = endereco.logradouro;
+    //Colocar o valor de logradouro da API dentro do campo logradouro do formulário
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('uf').value = endereco.uf;
+}
